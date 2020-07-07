30c2eca4cc2eeda63356021515d77caea6e3f526
Aapo Laaksonen <aapo.laaksonen@idealo.de>
2020-07-07 08:54:25 +0200

    - JavaLoopsI

--- src/main/java/hackerrank/JavaLoopsI.java
+++ src/main/java/hackerrank/JavaLoopsI.java
@@ -1,5 +1,19 @@
 // https://www.hackerrank.com/challenges/java-loops-i/problem
 package hackerrank;
 
+import java.util.Scanner;
+
 public class JavaLoopsI {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for(int i=1; i<=10; i++){
+            System.out.println(N + " x " + i + " = " + i*N);
+        }
+        scanner.close();
+        }
 }
