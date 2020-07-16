08278958889483d0c3a772285ce4eeb6d9f6e7db
Aapo Laaksonen <aapo.laaksonen@idealo.de>
2020-07-16 16:07:29 +0200

    - JavaEndOfFile

---
+++ src/main/java/hackerrank/JavaEndOfFile.java
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/java-datatypes/problem
+package hackerrank;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class JavaEndOfFile {
+    public static void main(String []args)
+    {
+        ArrayList<String> file = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+         file.add(sc.nextLine());
+        }
+        int lineNumber = 0;
+        for (String line: file) {
+            lineNumber++;
+            System.out.println(lineNumber + " " + line);
+        }
+    }
+}
+
