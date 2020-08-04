432c5a2a67e75f456671f4f05be72dcd0c5baef9
Aapo Laaksonen <aapo.laaksonen@idealo.de>
2020-08-04 08:43:21 +0200

    - JavaStringReverse

---
+++ src/main/java/hackerrank/JavaStringReverse.java
@@ -0,0 +1,21 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class JavaStringReverse {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+
+        String reverse = new StringBuilder(A).reverse().toString();
+        if(reverse.equalsIgnoreCase(A)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
+
